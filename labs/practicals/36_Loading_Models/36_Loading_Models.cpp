43acf35155f65734bfa48bb52f413d73fb5a28a3
Timothy Sanderson <40219124@live.napier.ac.uk>
2017-02-20 13:40:36 +0000

    finished loading models and added some meshes from blender.

--- labs/practicals/36_Loading_Models/36_Loading_Models.cpp
+++ labs/practicals/36_Loading_Models/36_Loading_Models.cpp
@@ -11,64 +11,65 @@ mesh m;
 target_camera cam;
 
 bool load_content() {
-  // *********************************
-  // Load in model, models/teapot.obj
+	// *********************************
+	// Load in model, models/teapot.obj
+	m = mesh(geometry("models/Fexit light_02.obj"));
+	// Load in texture, textures/checker.png
+	tex = texture("textures/sign.jpg");
+	// *********************************
+	m.get_transform().scale = vec3(20.0f);
 
-  // Load in texture, textures/checker.png
+	// Load in shaders
+	eff.add_shader("27_Texturing_Shader/simple_texture.vert", GL_VERTEX_SHADER);
+	eff.add_shader("27_Texturing_Shader/simple_texture.frag", GL_FRAGMENT_SHADER);
+	// Build effect
+	eff.build();
 
-  // *********************************
+	// Set camera properties
+	cam.set_position(vec3(200.0f, 200.0f, 200.0f));
+	cam.set_target(vec3(0.0f, 0.0f, 0.0f));
 
-  // Load in shaders
-  eff.add_shader("27_Texturing_Shader/simple_texture.vert", GL_VERTEX_SHADER);
-  eff.add_shader("27_Texturing_Shader/simple_texture.frag", GL_FRAGMENT_SHADER);
-  // Build effect
-  eff.build();
-
-  // Set camera properties
-  cam.set_position(vec3(200.0f, 200.0f, 200.0f));
-  cam.set_target(vec3(0.0f, 0.0f, 0.0f));
-
-  cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
-  return true;
+	cam.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
+	return true;
 }
 
 bool update(float delta_time) {
-  // Update the camera
-  cam.update(delta_time);
-  return true;
+	// Update the camera
+	cam.update(delta_time);
+	return true;
 }
 
 bool render() {
-  // Bind effect
-  renderer::bind(eff);
-  // Create MVP matrix
-  auto M = m.get_transform().get_transform_matrix();
-  auto V = cam.get_view();
-  auto P = cam.get_projection();
-  auto MVP = P * V * M;
-  // Set MVP matrix uniform
-  glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
+	// Bind effect
+	renderer::bind(eff);
+	// Create MVP matrix
+	auto M = m.get_transform().get_transform_matrix();
+	auto V = cam.get_view();
+	auto P = cam.get_projection();
+	auto MVP = P * V * M;
+	// Set MVP matrix uniform
+	glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
 
-  // *********************************
-  // Bind texture to renderer
+	// *********************************
+	// Bind texture to renderer
+	renderer::bind(tex, 0);
+	// Set the texture value for the shader here
+	glUniform1i(eff.get_uniform_location("tex"), 0);
+	// *********************************
 
-  // Set the texture value for the shader here
+	// Render mesh
+	renderer::render(m);
 
-  // *********************************
-
-  // Render mesh
-  renderer::render(m);
-
-  return true;
+	return true;
 }
 
 void main() {
-  // Create application
-  app application("36_Loading_Models");
-  // Set load content, update and render methods
-  application.set_load_content(load_content);
-  application.set_update(update);
-  application.set_render(render);
-  // Run application
-  application.run();
+	// Create application
+	app application("36_Loading_Models");
+	// Set load content, update and render methods
+	application.set_load_content(load_content);
+	application.set_update(update);
+	application.set_render(render);
+	// Run application
+	application.run();
 }
