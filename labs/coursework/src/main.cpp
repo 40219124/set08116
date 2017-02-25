93b3045ed9e8ee4601eb76396311e79c062b3714
TIM-DESKTOP\Tim <t.s.sanderson@hotmail.co.uk>
2017-02-25 03:10:08 +0000

    A good amount of work on implementing other cameras. Some problems with free cam and setting the target though.

--- labs/coursework/src/main.cpp
+++ labs/coursework/src/main.cpp
@@ -14,7 +14,11 @@ spot_light sLight;
 texture tex;
 effect eff;
 target_camera target_c;
+free_camera free_c;
+uint cam_state = 0;
 float time_total = 0.0f;
+double mouse_x;
+double mouse_y;
 
 bool load_content() {
 	// Loads in a texture
@@ -68,16 +72,136 @@ bool load_content() {
 
 
 
-	// Set camera properties
+	// Set target camera properties
 	target_c.set_position(vec3(0.0f, 20.0f, 20.0f));
 	target_c.set_target(vec3(0.0f, 0.0f, 0.0f));
 	target_c.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
+	// Set free camera properties
+	free_c.set_position(vec3(20.0f, 20.0f, -20.0f));
+	free_c.set_target(vec3(0.0f, 0.0f, 0.0f));
+	free_c.set_projection(quarter_pi<float>(), renderer::get_screen_aspect(), 0.1f, 1000.0f);
+
 	return true;
 }
 
+bool initialise() {
+	glfwGetCursorPos(renderer::get_window(), &mouse_x, &mouse_y);
+	glfwSetInputMode(renderer::get_window(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	return true;
+}
+
+//all things relating to target cam manipulation
+void target_manipulation(float delta_time) {
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_1)) {
+		target_c.set_position(vec3(0.0f, 20.0f, 20.0f));
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_2)) {
+		target_c.set_position(vec3(20.0f, 10.0f, 20.0f));
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_3)) {
+		target_c.set_position(vec3(20.0f, 10.0f, -20.0f));
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_4)) {
+		target_c.set_position(vec3(-20.0f, 10.0f, -20.0f));
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_EQUAL)) {
+		target_c.set_position(target_c.get_target() - (1 - 0.2f * delta_time) * (target_c.get_target() - target_c.get_position()));
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_MINUS)) {
+		target_c.set_position(target_c.get_target() - (1 + 0.25f * delta_time) * (target_c.get_target() - target_c.get_position()));
+	}
+	target_c.update(delta_time);
+}
+
+void free_manipulation(float delta_time) {
+
+	static const float screen_w = static_cast<float>(renderer::get_screen_width());
+	static const float screen_h = static_cast<float>(renderer::get_screen_height());
+	static const double ratio_w = quarter_pi<float>() / screen_w;
+	static const double ratio_h = (quarter_pi<float>() * (screen_h / screen_w)) / screen_h;
+	
+	double current_x;
+	double current_y;
+	glfwGetCursorPos(renderer::get_window(), &current_x, &current_y);
+
+	double delta_x = current_x - mouse_x;
+	double delta_y = current_y - mouse_y;
+
+	delta_x *= ratio_w;
+	delta_y *= ratio_h;
+
+	free_c.rotate(delta_x, -delta_y);
+
+	float speed = 10.0f * delta_time;
+	vec3 mov = vec3(0.0f);
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_W)) {
+		mov += vec3(0.0f, 0.0f, 1.0f);
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_S)) {
+		mov += vec3(0.0f, 0.0f, -1.0f);
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_A)) {
+		mov += vec3(-1.0f, 0.0f, 0.0f);
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_D)) {
+		mov += vec3(1.0f, 0.0f, 0.0f);
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_LEFT_SHIFT)) {
+		mov += vec3(0.0f, -1.0f, 0.0f);
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_SPACE)) {
+		mov += vec3(0.0f, 1.0f, 0.0f);
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_LEFT_CONTROL)) {
+		speed *= 3.0f;
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_H)) {
+		free_c.set_target(vec3(0.0f, 0.0f, 0.0f) - free_c.get_position());
+	}
+
+	if (mov != vec3(0.0f)) {
+		mov = speed * normalize(mov);
+		free_c.move(mov);
+	}
+
+	mouse_x = current_x;
+	mouse_y = current_y;
+
+	free_c.update(delta_time);
+}
 
 bool update(float delta_time) {
 	// Update the camera
+
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_Z)) {
+		cam_state = 0; //target cam
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_X)) {
+		cam_state = 1; //free cam
+	}
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_C)) {
+		cam_state = 2; //arc ball cam
+	}
+
+	switch (cam_state)
+	{
+	case (0):
+		target_manipulation(delta_time);
+		break;
+	case (1):
+		free_manipulation(delta_time);
+		break;
+	default:
+		cam_state = 0;
+		break;
+	}
+
+
+
+
+
+
 
 	//sphere manipulation maths
 	time_total += delta_time;
@@ -101,7 +225,6 @@ bool update(float delta_time) {
 	//for loop to access each sphere and transform it
 	for (int i = 0; i < spheres; ++i) {
 		name = ("sphere" + to_string(i + 1));
-		
 		//maths for spherical wibbling
 		rotq = meshes[name].get_transform().orientation;
 		rotq = rotate(rotq, amplitude * sin((i / spheres) * waves_per_circle * full_circle + time_total * wibble_speed), vec3(0.0f, 0.0f, 1.0f)); //rotates around z axis to give effect of a sphere
@@ -118,10 +241,9 @@ bool update(float delta_time) {
 		0.0f,
 		0.0f);*/
 
-	//rotate centre (parent) sphere
+		//rotate centre (parent) sphere
 	meshes["sphere0"].get_transform().rotate(eulerAngleY(delta_time * circling_speed));
 
-	target_c.update(delta_time);
 	return true;
 }
 
@@ -155,6 +277,29 @@ mat3 transformHierarchyN(string first, mat3 N) {
 	}
 }
 
+void renderCams(mat4* V, mat4* P) {
+	switch (cam_state) {
+	case(0):
+		*V = target_c.get_view();
+		*P = target_c.get_projection();
+		//set camera location, eye_pos
+		glUniform3fv(eff.get_uniform_location("eye_pos"), 1, value_ptr(target_c.get_position()));
+		break;
+	case(1):
+		*V = free_c.get_view();
+		*P = free_c.get_projection();
+		//set camera location, eye_pos
+		glUniform3fv(eff.get_uniform_location("eye_pos"), 1, value_ptr(free_c.get_position()));
+		break;
+	default:
+		*V = target_c.get_view();
+		*P = target_c.get_projection();
+		//set camera location, eye_pos
+		glUniform3fv(eff.get_uniform_location("eye_pos"), 1, value_ptr(target_c.get_position()));
+		break;
+	}
+}
+
 bool render() {
 	// Bind effect
 	renderer::bind(eff);
@@ -166,8 +311,10 @@ bool render() {
 		mat4 M = mat4(1.0f);
 		// get the hierarchy of transformations associated with the object
 		M = transformHierarchyM(item.first, M);
-		mat4 V = target_c.get_view();
-		mat4 P = target_c.get_projection();
+
+		mat4 V;
+		mat4 P;
+		renderCams(&V, &P);
 		mat4 MVP = P * V * M;
 		// Set MVP matrix uniform
 		glUniformMatrix4fv(eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
@@ -178,9 +325,6 @@ bool render() {
 		N = transformHierarchyN(item.first, N);
 		glUniformMatrix3fv(eff.get_uniform_location("N"), 1, GL_FALSE, value_ptr(N));
 		
-		//set camera location, eye_pos
-		glUniform3fv(eff.get_uniform_location("eye_pos"), 1, value_ptr(target_c.get_position()));
-
 		//bind the texture which shares the name of the mesh
 		renderer::bind(*texs[item.first], 0);
 		glUniform1i(eff.get_uniform_location("tex"), 0);
@@ -200,6 +344,7 @@ void main() {
 	app application("Graphics Coursework");
 	// Set load content, update and render methods
 	application.set_load_content(load_content);
+	application.set_initialise(initialise);
 	application.set_update(update);
 	application.set_render(render);
 	cout << "\\o\n |\\\n /\\" << endl;
